1d88addf652487d2a8b4a62823539b9400186ce9
Dmytro Sydorenko <dima83718@gmail.com>
2023-09-26 14:48:11 +0300

    add DZ3

---
+++ Dz3/DC1.rb
@@ -0,0 +1,91 @@
+# Компонент - базовий клас для замовлення
+class Order
+  def description
+    'Замовлення'
+  end
+
+  def cost
+    0
+  end
+end
+
+# Конкретний компонент - страва в меню
+class Dish < Order
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def description
+    "Страва: #{@name}"
+  end
+
+  def cost
+    @price
+  end
+end
+
+# Декоратор - базовий клас декоратора для замовлення
+class OrderDecorator < Order
+  attr_reader :order
+
+  def initialize(order)
+    @order = order
+  end
+
+  def description
+    @order.description
+  end
+
+  def cost
+    @order.cost
+  end
+end
+
+# Конкретний декоратор - додавання напою до замовлення
+class BeverageDecorator < OrderDecorator
+  attr_reader :beverage_name, :beverage_price
+
+  def initialize(order, beverage_name, beverage_price)
+    super(order)
+    @beverage_name = beverage_name
+    @beverage_price = beverage_price
+  end
+
+  def description
+    "#{super}, Напій: #{@beverage_name}"
+  end
+
+  def cost
+    super + @beverage_price
+  end
+end
+
+# Конкретний декоратор - додавання десерту до замовлення
+class DessertDecorator < OrderDecorator
+  attr_reader :dessert_name, :dessert_price
+
+  def initialize(order, dessert_name, dessert_price)
+    super(order)
+    @dessert_name = dessert_name
+    @dessert_price = dessert_price
+  end
+
+  def description
+    "#{super}, Десерт: #{@dessert_name}"
+  end
+
+  def cost
+    super + @dessert_price
+  end
+end
+
+# Використання
+order = Dish.new('Стейк', 25)
+order_with_beverage = BeverageDecorator.new(order, 'Кава', 5)
+order_with_dessert = DessertDecorator.new(order_with_beverage, 'Чізкейк', 8)
+
+puts order_with_dessert.description # Виведе: "Страва: Стейк, Напій: Кава, Десерт: Чізкейк"
+puts order_with_dessert.cost        # Виведе: 38
---
+++ Dz3/DC2.rb
@@ -0,0 +1,54 @@
+# Компонент - базовий клас для музичного плеєра
+class MusicPlayer
+  def play
+    # Логіка відтворення музики
+    raise NotImplementedError, 'Потрібно реалізувати метод play у підкласах'
+  end
+end
+
+# Конкретний компонент - музичний файл
+class MusicFile < MusicPlayer
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def play
+    puts "Відтворення музичного файлу: #{@filename}"
+  end
+end
+
+# Декоратор - базовий клас декоратора для збереження та завантаження даних
+class DataDecorator < MusicPlayer
+  def initialize(music_player)
+    @music_player = music_player
+  end
+
+  def play
+    @music_player.play
+  end
+end
+
+# Конкретний декоратор - збереження у форматі MP3
+class MP3Decorator < DataDecorator
+  def save
+    puts "Збереження у форматі MP3: #{@filename}.mp3"
+  end
+end
+
+# Конкретний декоратор - збереження у форматі WAV
+class WAVDecorator < DataDecorator
+  def save
+    puts "Збереження у форматі WAV: #{@filename}.wav"
+  end
+end
+
+# Використання
+music_file = MusicFile.new("song")
+mp3_player = MP3Decorator.new(music_file)
+wav_player = WAVDecorator.new(music_file)
+
+mp3_player.play  # Відтворення музичного файлу: song
+mp3_player.save  # Збереження у форматі MP3: song.mp3
+
+wav_player.play  # Відтворення музичного файлу: song
+wav_player.save  # Збереження у форматі WAV: song.wav

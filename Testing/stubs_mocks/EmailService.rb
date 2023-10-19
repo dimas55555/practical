c89037ddbca61b35b27321495f8804cc75b164dd
Dmytro Sydorenko <dima83718@gmail.com>
2023-10-19 13:46:29 +0300

    add mock

--- Testing/stubs_mocks/EmailService.rb
+++
@@ -1,9 +0,0 @@
-class EmailService
-  def initialize(email_client)
-    @email_client = email_client
-  end
-
-  def send_email(to, subject, body)
-    @email_client.send_email(to, subject, body)
-  end
-end

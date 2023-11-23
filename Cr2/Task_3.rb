e29f147dfd2c94ccec30a602de5a2e7451268b36
Dmytro Sydorenko <dima83718@gmail.com>
2023-11-23 16:51:13 +0200

    ADD CR2

--- Cr2/Task_3.rb
+++ Cr2/Task_3.rb
@@ -1,12 +1,17 @@
+# Модуль Printable містить print_info, що виводить інформацію про об'єкт
 module Printable
   def print_info
+    # Виводимо заголовок інформації про об'єкт: клас та ідентифікатор об'єкта
     puts "Інформація про об'єкт (Клас:#{self.class} - ID: #{object_id}):"
+
+    # Для кожної інстанційної змінної виводимо її ім'я та значення
     instance_variables.each do |var|
       puts "#{var}: #{instance_variable_get(var)}"
     end
   end
 end
 
+# Клас Person містить ім'я, стать та вік
 class Person
   include Printable
 
@@ -17,6 +22,7 @@ class Person
   end
 end
 
+# Клас Book має назву та кількість сторінок
 class Book
   include Printable
   attr_accessor :title, :pages
@@ -27,8 +33,10 @@ class Book
   end
 end
 
-person = Person.new("Брендон Вайт","Ч", 35)
+# Створюємо об'єкт класу Person і виводимо інформацію про нього
+person = Person.new("Брендон Вайт", "Ч", 35)
 person.print_info
 
+# Створюємо об'єкт класу Book і виводимо інформацію про нього
 book = Book.new("Мова програмування Рубі", 465)
 book.print_info
